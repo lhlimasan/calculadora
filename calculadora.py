5e4b5aea66bc61987605c85144cfffe28e0ce799
Luis Henrique <128630495+lhlimasan@users.noreply.github.com>
2024-01-24 19:07:59 -0300

    calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+def calculadora(num1, num2, operacao):
+    if operacao == 1:
+        return num1 + num2
+    elif operacao == 2:
+        return num1 - num2
+    elif operacao == 3:
+        return num1 * num2
+    elif operacao == 4:
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Erro: Divisão por zero"
+    else:
+        return 0
+
+# Exemplo da função
+resultado = calculadora(10, 5, 1)
+print(resultado)
